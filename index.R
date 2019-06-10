2801b6f951ba4005a0da60eb49ef9b73956ed590
Daniel Freeman <danielfreeman@daniels-air.home>
2019-06-10 10:50:40 -0400

    added index.R

---
+++ index.R
@@ -0,0 +1,120 @@
+# Load libraries.
+library(spatstat)
+library(tidyverse)
+
+# Specify paths.
+coord_path <- "/Users/danielfreeman/Desktop/ag/coord.csv" # csv file created by work_data.py
+image_path <- "/Users/danielfreeman/Desktop/ag/Cropped/" # directory containing cropped images
+to_path <- "/Users/danielfreeman/Desktop/ag/Split_all_species/" # directory where train and test sets will be stored
+
+# Upload coordinates.
+dat <- read.csv(coord_path)
+colnames(dat) <- c("dataset", "image", "species", "condition", "x_start", "y_start", "width", "height", "id")
+
+# Calculate the center of each bounding box.
+dat$x_center <- dat$x_start + dat$width/2
+dat$y_center <- dat$y_start + dat$height/2
+
+# Temp.
+dat <- dat[dat$dataset != 'FLT1_2',] # remove images from dataset 'FLT1_2'
+dat <- dat[dat$condition != 'unknown',] # remove phosphorus stress images
+dat <- dat[dat$condition != 'low_water_stress',] # remove phosphorus stress images
+dat$dataset <- as.character(dat$dataset) # manually assign MAPIR images to FLT1 or FLT2
+dat$dataset[dat$image %in% c("2017_0810_145123_112.JPG", "2017_0810_145127_114.JPG",
+                           "2017_0810_145131_116.JPG", "2017_0810_145135_118.JPG",
+                           "2017_0810_145139_120.JPG")] <- 'MAPIR_FLT1'
+dat$dataset[dat$image %in% c("2017_0810_154133_084.JPG", "2017_0810_154137_086.JPG",
+                           "2017_0810_154142_088.JPG")] <- 'MAPIR_FLT2'
+# dat$condition <- as.character(dat$condition) # pool HWS and LWS images
+# dat$condition[dat$condition == 'high_water_stress'] <- 'stress'
+# dat$condition[dat$condition == 'low_water_stress'] <- 'stress'
+# dat$species <- 'pool' # pool species
+
+# Sort cropped images by position and assign to k-fold train and test sets.
+df <- data.frame()
+# Loop through each combination of species and condition.
+dat$model <- paste(dat$dataset, dat$species, dat$condition, sep = "_")
+for(model in split(dat, dat$model)) {
+  # Normalize coordinates.
+  model$x_norm <- (model$x_center - min(model$x_center)) / (max(model$x_center) - min(model$x_center))
+  model$y_norm <- (model$y_center - min(model$y_center)) / (max(model$y_center) - min(model$y_center))
+  # Select a reference image.
+  ref_img <- model$image[1]
+  ref <- model[model$image == ref_img,]
+  ppp1 <- ppp(ref$x_norm, ref$y_norm)
+  # Loop through each image.
+  for(image in split(model, model$image)) {
+    # Skip empty datafames.
+    if(nrow(image) == 0) {next}
+    # Report error and skip if number of points don't match.
+    if(nrow(image) != nrow(ref)) {
+      warning(
+        "Number of points doesn't match \n",
+        "Species:", as.character(ref$species[1]), "\n",
+        "Condition:", as.character(ref$condition[1]), "\n",
+        "Reference:", as.character(ref$image[1]), "\n",
+        "Points:", nrow(ref), "\n",
+        "Target:", as.character(image$image[1]), "\n",
+        "Points:", nrow(image), "\n\n"
+      )
+      next
+    }
+    # Match points with reference image.
+    ppp2 <- ppp(image$x_norm, image$y_norm)
+    match <- pppdist(ppp1, ppp2)
+    mx <- matrix(match)[3,][[1]]
+    image$position <- apply(mx, 1, function(x) {which(x == 1)})
+    # Sort cropped images by position.
+    image <- image[order(image$position),]
+    # Split observations into k-fold test and training sets.
+    folds <- cut(seq(1:nrow(image)), breaks = 4, labels = F)
+    for(k in 1:4) {
+      image[paste0("train_k", k)] <- folds != k
+      image[paste0("test_k", k)] <- folds == k
+    }
+    # Concatenate image dataframe.
+    df <- rbind(df, image)
+  }
+}
+
+# Append path to each cropped image.
+files <- list.files(image_path, full.names = T, recursive = T)
+for(i in 1:nrow(df)) {
+  end <- paste0("_", df$id[i], ".JPG")[1]
+  df$path[i] <- files[endsWith(files, end)][1]
+}
+
+# Create parent directory if none exists.
+if(!dir.exists(to_path)) {
+  dir.create(to_path)
+}
+
+# Create directory for each model.
+setwd(to_path)
+# Subset images to be used in each model.
+for(model in unique(df$model)) {
+  cat(paste("Splitting model:", model, "\n"))
+  subset <- df[df$model == model,]
+  # Sort by position so that related images appear together if sorted by date.
+  subset <- subset[order(subset$position),]
+  # Loop through each fold.
+  for(k in 1:4) {
+    # Define directory names.
+    train_dir <- paste0(model, "_k", k, "_train")
+    test_dir <- paste0(model, "_k", k, "_test")
+    # Create directories.
+    dir.create(train_dir)
+    dir.create(test_dir)
+    # Get indices of training and test set images.
+    train_index <- subset[[paste0("train_k", k)]]
+    test_index <- subset[[paste0("test_k", k)]]
+    # Copy images.
+    file.copy(subset$path[train_index], train_dir)
+    file.copy(subset$path[test_index], test_dir)
+    # Zip training and test sets.
+    zip(zipfile = train_dir, files = train_dir)
+    zip(zipfile = test_dir, files = test_dir)
+  }
+}
+
+#View(df[,c(1:4,9,14:24)])
--- predict.py
+++ predict.py
@@ -30,13 +30,9 @@ def train_model(model_name, ns_train, stress_train):
 def wait(modelID):
     # Loop indefinitely.
     while True:
-        # Get status of all models.
-        classifiers = visual_recognition.list_classifiers(verbose=True).get_result()
-        # Get status of specific model.
-        for c in classifiers['classifiers']:
-            if c['classifier_id'] == modelID:
-                status = c['status']  
-                break
+        # Get status.
+        classifier = visual_recognition.get_classifier(modelID).get_result()
+        status = classifier['status']
         # If ready, break loop.
         if status == 'ready':
             print(modelID, 'ready')
@@ -46,17 +42,17 @@ def wait(modelID):
             print('Pinged', modelID, time.ctime())
             time.sleep(30)
             continue
-        # If failed, return explanation.
+        # If failed, print explanation.
         if status == 'failed':
             print(modelID, 'failed')
-            print(c['explanation'])
+            print(classifier['explanation'])
             break
         # Temp.
         print(modelID)
         print(status)
 
 # Test zip.
-def test_images(modelID, test_path):
+def test_zip(modelID, test_path):
     with open(test_path, 'rb') as images_file:
         response = visual_recognition.classify(
             images_file,
@@ -70,15 +66,11 @@ def res_to_df(response):
     df = []
     # Loop through images.
     for img in response['images']:
-        # Get identifying information.
+        # Get image information.
         file = img['image']
-        model = img['classifiers'][0]['name']
-        # Get score for stress class.
-        result = img['classifiers'][0]['classes']
-        for c in result:
-            if c['class'] == 'stress':
-                score = c['score']
-        # Append to dataframe..
+        model = img['classifiers'][0]['classifier_id']
+        score = img['classifiers'][0]['classes'][0]['score']
+        # Append to dataframe.
         df.append({'image': file, 'model': model, 'score': score})
     # Convert list to dataframe and return.
     df = pd.DataFrame(df)
@@ -89,11 +81,11 @@ def res_to_df(response):
 def test_multiple(modelID, stress_test, ns_test):
     print("Testing model", modelID)
     # Test stress images.
-    stress_res = test_images(modelID, stress_test)
+    stress_res = test_zip(modelID, stress_test)
     stress_df = res_to_df(stress_res)
     stress_df['stress'] = True
     # Test no_stress images.
-    ns_res = test_images(modelID, ns_test)
+    ns_res = test_zip(modelID, ns_test)
     ns_df = res_to_df(ns_res)
     ns_df['stress'] = False
     # Concatenate dataframes.
@@ -135,38 +127,40 @@ def performance(df):
 def save(dic, path):
     # If csv exists, open as dataframe and append.
     if os.path.exists(path):
-        df = pd.read_csv(path)
+        df = pd.read_csv(path, index_col=0)
         df = df.append(dic, ignore_index = True)
     # If csv doesn't exist, convert dictionary to dataframe.
     else:
-        df = pd.DataFrame.from_dict(dic, orient = 'index').T
+        df = pd.DataFrame.from_dict(dic, orient='index').T
     # Save dataframe.
     df.to_csv(path)
 
 # Wrapper function trains model and returns modelID.
-def pipeline_train(model_name, stress_train, ns_train, parent = os.getcwd()):
+def pipeline_train(model_name, stress_train, ns_train, log, parent = os.getcwd()):
     #
     # Args:
     #   model_name: what to name the model.
-    #   stress_train, ns_train: paths to zip files containing training sets of
-    #   'stress' and 'no stress' images.
+    #   stress_train, ns_train: paths to zip files containing training set images.
+    #   perf_save: path to csv file to which model information will be appended.
     #   parent: working directory (default to current).
     #
     # Change directory.
     os.chdir(parent)
     # Train model.
     modelID = train_model(model_name, ns_train, stress_train)['classifier_id']
+    # Update training log.
+    log = {'model': modelID, 'stress_train': stress_train, 'ns_train': ns_train,
+           'time': time.ctime()}
+    save(log, 'Results/Log.csv')
     # Return modelID.
     return(modelID)
     
 # Wrapper function tests model and returns performance metrics.
-def pipeline_test(modelID, stress_train, ns_train, stress_test, ns_test,
-                  pred_save, perf_save, trained):
+def pipeline_test(modelID, stress_test, ns_test, pred_save, perf_save, trained):
     #
     # Args:
     #   modelID: modelID returned by 'pipeline_train'
-    #   stress_test, ns_test: paths to zip files containing test and training
-    #   sets of 'stress' and 'no stress' images.
+    #   stress_test, ns_test: paths to zip files containing test set images.
     #   pred_save: path to directory where image-level predictions will be saved.
     #   perf_save: path to csv file to which model performance metrics will be appended.
     #   trained: path to directory where images will be moved to after use.
@@ -178,41 +172,103 @@ def pipeline_test(modelID, stress_train, ns_train, stress_test, ns_test,
     pred.to_csv(full_path)
     # Calculate and return performance metrics.
     perf = performance(pred)
-    return(perf)
     # Append image paths and timestamp.
-    perf['stress_train'] = stress_train
-    perf['ns_train'] = ns_train
     perf['stress_test'] = stress_test
     perf['ns_test'] = ns_test 
     perf['time'] = time.ctime()
     # Append to 'perf_save' csv.
     save(perf, perf_save)
     # Moved images to 'trained' directory.
-    if trained != '':
-        os.rename(stress_train, os.path.join(trained, os.path.basename(stress_train)))
-        os.rename(ns_train, os.path.join(trained, os.path.basename(ns_train)))
-        os.rename(stress_test, os.path.join(trained, os.path.basename(stress_test)))
-        os.rename(ns_test, os.path.join(trained, os.path.basename(ns_test)))
-    print('Done')
+#    if trained != '':
+#        os.rename(stress_train, os.path.join(trained, os.path.basename(stress_train)))
+#        os.rename(ns_train, os.path.join(trained, os.path.basename(ns_train)))
+#        os.rename(stress_test, os.path.join(trained, os.path.basename(stress_test)))
+#        os.rename(ns_test, os.path.join(trained, os.path.basename(ns_test)))
+    # Return performance metrics.
+    return(perf)
 
 # Example ----
 
-# MAPIR Buddeleia: NS v. HWS.
-model_name = 'MAPIR_bud_HWS_k1'
+# MAPIR Buddeleia: NS v. HWS k1 (tested).
+# MAPIR Buddeleia: NS v. HWS k2 (tested).
+model_name = 'MAPIR_bud_HWS_k2'
 parent = '/Users/danielfreeman/Desktop/ag/'
-stress_train = 'Split/MAPIR_FLT2_Buddleia_high_water_stress_k1_train.zip'
-ns_train = 'Split/MAPIR_FLT2_Buddleia_no_stress_k1_train.zip'
-stress_test = 'Split/MAPIR_FLT2_Buddleia_high_water_stress_k1_test.zip'
-ns_test = 'Split/MAPIR_FLT2_Buddleia_no_stress_k1_test.zip'
+stress_train = 'Split/MAPIR_FLT2_Buddleia_high_water_stress_k2_train.zip'
+ns_train = 'Split/MAPIR_FLT2_Buddleia_no_stress_k2_train.zip'
+stress_test = 'Split/MAPIR_FLT2_Buddleia_high_water_stress_k2_test.zip'
+ns_test = 'Split/MAPIR_FLT2_Buddleia_no_stress_k2_test.zip'
 pred_save = 'Results/'
 perf_save = 'Results/performance.csv'
 trained = 'Trained/'
-modelID = pipeline_train(model_name, stress_train, ns_train, parent)
-#modelID = MAPIR_bud_HWS_k1_1318838362
+#modelID = pipeline_train(model_name, stress_train, ns_train, log, parent)
+#modelID = 'MAPIR_bud_HWS_k1_1318838362'
+modelID = 'MAPIR_bud_HWS_k2_1941215476'
 wait(modelID)
 perf = pipeline_test(modelID, stress_train, ns_train, stress_test, ns_test,
                      pred_save, perf_save, trained)
 perf
 
-# MAPIR_bud_HWS_k1_257413000 # two classes
-# MAPIR_bud_HWS_k1_1318838362 # negative class
+# MAPIR HQ: NS v. HWS k1 (tested).
+# MAPIR HQ: NS v. HWS k2 (tested).
+# MAPIR HQ: NS v. HWS k3 (tested).
+# MAPIR HQ: NS v. HWS k4 (tested).
+model_name = 'MAPIR_hq_HWS_k4'
+parent = '/Users/danielfreeman/Desktop/ag/'
+stress_train = 'Split/MAPIR_FLT1_Hydrangeo quercifolia_high_water_stress_k4_train.zip'
+ns_train = 'Split/MAPIR_FLT1_Hydrangeo quercifolia_no_stress_k4_train.zip'
+stress_test = 'Split/MAPIR_FLT1_Hydrangeo quercifolia_high_water_stress_k4_test.zip'
+ns_test = 'Split/MAPIR_FLT1_Hydrangeo quercifolia_no_stress_k4_test.zip'
+pred_save = 'Results/'
+perf_save = 'Results/performance.csv'
+trained = 'Trained/'
+#modelID = pipeline_train(model_name, stress_train, ns_train, parent)
+#modelID = 'MAPIR_hq_HWS_k1_1329753609' 
+#modelID = 'MAPIR_hq_HWS_k1_1315858108' # should be k2
+#modelID = 'MAPIR_hq_HWS_k3_240431902'
+#modelID = 'MAPIR_hq_HWS_k4_1413776194'
+wait(modelID)
+perf = pipeline_test(modelID, stress_train, ns_train, stress_test, ns_test,
+                     pred_save, perf_save, trained)
+perf
+
+# MAPIR HQ: NS v. S k1 (tested).
+# MAPIR HQ: NS v. S k2 (tested).
+# MAPIR HQ: NS v. S k3 (tested).
+# MAPIR HQ: NS v. S k4 (tested).
+model_name = 'MAPIR_hq_k4'
+parent = '/Users/danielfreeman/Desktop/ag/'
+stress_train = 'Split_all_stress/MAPIR_FLT1_Hydrangeo quercifolia_stress_k4_train.zip'
+ns_train = 'Split_all_stress/MAPIR_FLT1_Hydrangeo quercifolia_no_stress_k4_train.zip'
+stress_test = 'Split_all_stress/MAPIR_FLT1_Hydrangeo quercifolia_stress_k4_test.zip'
+ns_test = 'Split_all_stress/MAPIR_FLT1_Hydrangeo quercifolia_no_stress_k4_test.zip'
+pred_save = 'Results/'
+perf_save = 'Results/performance.csv'
+trained = 'Trained/'
+#modelID = pipeline_train(model_name, stress_train, ns_train, parent)
+#modelID = 'MAPIR_hq_k1_279069122'
+#modelID = 'MAPIR_hq_k2_837039308'
+#modelID = 'MAPIR_hq_k3_1948259550'
+#modelID = 'MAPIR_hq_k4_1093481789'
+wait(modelID)
+perf = pipeline_test(modelID, stress_train, ns_train, stress_test, ns_test,
+                     pred_save, perf_save, trained)
+perf
+
+# MAPIR ALL: NS v. S k1 (tested).
+# MAPIR ALL: NS v. S k2 (tested).
+model_name = 'MAPIR_all_k2'
+parent = '/Users/danielfreeman/Desktop/ag/'
+stress_train = 'Split_all_species/MAPIR_FLT1_pool_stress_k2_train.zip'
+ns_train = 'Split_all_species/MAPIR_FLT1_pool_no_stress_k2_train.zip'
+stress_test = 'Split_all_species/MAPIR_FLT1_pool_stress_k2_test.zip'
+ns_test = 'Split_all_species/MAPIR_FLT1_pool_no_stress_k2_test.zip'
+pred_save = 'Results/'
+perf_save = 'Results/performance.csv'
+trained = 'Trained/'
+#modelID = pipeline_train(model_name, stress_train, ns_train, parent)
+#modelID = 'MAPIR_all_k1_134415419'
+#modelID = 'MAPIR_all_k2_887808858'
+wait(modelID)
+perf = pipeline_test(modelID, stress_train, ns_train, stress_test, ns_test,
+                     pred_save, perf_save, trained)
+perf
